6e8092bb83e96146243386c70c69e8b8a3ab086b
Divyansh Kogta <42926672+divyanshk7@users.noreply.github.com>
2019-05-29 13:55:15 +0530

    Update RestaurantBill.java

--- src/RestaurantBill.java
+++ src/RestaurantBill.java
@@ -1,16 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author HP
- */
 public class RestaurantBill {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
